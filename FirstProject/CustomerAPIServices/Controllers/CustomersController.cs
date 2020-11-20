93d72d820bdb5e0320ce7f0fb16ade89431bb17c
Linh <vulinhit09@gmail.com>
2020-11-20 13:46:22 +0700

    them

---
+++ FirstProject/CustomerAPIServices/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerAPIServices.Controllers
+{
+    [Route("api/[Controller]")]
+    public class CustomersController : Controller
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Linh Nguyen", "Phu Nguyen" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Day la - {id}";
+        }
+    }
+}
---
+++ FirstProject/ProductsAPIServices/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsAPIServices.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+       public IEnumerable<string> Get()
+        {
+            return new string[] { "Book 1", "Book 2" };
+        }
+    }
+}
